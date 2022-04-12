f72c4946d24c8ce30d49f1f77d2537a4d31d14e0
16AReeves <98365963+16AReeves@users.noreply.github.com>
2022-04-12 09:32:23 -0400

    Update module 7 schema

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,4 +1,14 @@
+-- Delete tables, if they exist
+DROP TABLE departments IF EXISTS;
+DROP TABLE employees IF EXISTS;
+DROP TABLE dept_manager IF EXISTS;
+DROP TABLE salaries IF EXISTS;
+DROP TABLE dept_employees IF EXISTS;
+DROP TABLE titles IF EXISTS;
+
+
 -- Creating tables for PH-EmployeeDB
+
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -7,7 +17,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	 emp_no INT NOT NULL,
+     emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -17,43 +27,49 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_employees (
-	dept_no VARCHAR(4) NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	PRIMARY KEY (dept_no)
+     emp_no INT NOT NULL,
+     dept_no VARCHAR(4) NOT NULL,
+     from_date DATE NOT NULL,
+     to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+     PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY (emp_no)
+      emp_no INT NOT NULL,
+      title VARCHAR NOT NULL,
+      from_date DATE NOT NULL,
+      to_date DATE NOT NULL,
+      FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+      PRIMARY KEY (emp_no)
 );
 
+-- View all of the data
 SELECT * FROM departments;
+SELECT * FROM dept_employees;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
 
