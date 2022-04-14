5e5ea7e16c14180acae65ad447547e58cd590a41
Abigayle Reeves <16areeves@gmail.com>
2022-04-13 22:05:47 -0400

    Pushing some of the challenge code

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,10 +1,35 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Drop table, if needed
+DROP TABLE retirement_title;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+-- Create table of retiring employees by title
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_title
+FROM employees as e, titles as ti
+WHERE (e.emp_no = ti.emp_no)
+AND (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, ti.emp_no DESC;
+
+
+-- Use DISTINCT ON to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_title as rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+
+-- Retrieve number of employees retiring based on title
+SELECT COUNT(ut.title) as "Title Count", ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY "Title Count" DESC;
+
