0c39ff6749ec2954140adbb6410ba4aac6611792
16AReeves <98365963+16AReeves@users.noreply.github.com>
2022-04-14 10:28:23 -0400

    Update challenge code

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,5 +1,10 @@
+-- Deliverable 1:
+
 -- Drop table, if needed
 DROP TABLE retirement_title;
+DROP TABLE unique_titles;
+DROP TABLE retiring_titles;
+
 
 -- Create table of retiring employees by title
 SELECT e.emp_no,
@@ -32,4 +37,30 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY "Title Count" DESC;
+
+
+
+--Deliverable 2:
+
+-- Query to create a Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name
+	e.last_name
+	e.birth_date
+	de.from_date
+	de.to_date
+	t.title
+--INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN title as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+
+
+
 
