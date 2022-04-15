35b934c4b78681188269849510238eb7977fd586
Abigayle Reeves <16areeves@gmail.com>
2022-04-15 17:54:03 -0400

    Pushing challenge code and queries

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -45,17 +45,17 @@ ORDER BY "Title Count" DESC;
 -- Query to create a Mentorship Eligibility table
 
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
-	e.first_name
-	e.last_name
-	e.birth_date
-	de.from_date
-	de.to_date
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
 	t.title
---INTO mentorship_eligibilty
+INTO mentorship_eligibilty
 FROM employees as e
 INNER JOIN dept_employees as de
 ON (e.emp_no = de.emp_no)
-INNER JOIN title as t
+INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (de.to_date = '9999-01-01')
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
@@ -67,47 +67,52 @@ ORDER BY e.emp_no;
 -- Count how many can Mentor from each department:
 
 SELECT COUNT(me.title) AS "Title Count", me.title
---INTO mentor_list
+INTO mentor_list
 FROM mentorship_eligibilty AS me
 GROUP BY me.title
 ORDER BY "Title Count" DESC;
 
 
 -- Count the number of retiring employees by title and by year:
-
-SELECT COUNT(ut.title) as "Title Count", 
+SELECT e.emp_no,
+	e.birth_date,
 	ut.title
-	e.birth_date
---INTO 1952_titles
-FROM unique_titles as ut, employees as e
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1952-12-31')
-GROUP BY ut.title
-ORDER BY "Title Count" DESC;
+INTO employee_titles
+FROM employees as e
+INNER JOIN unique_titles as ut
+ON (e.emp_no = ut.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+GROUP BY ut.title, e.emp_no
+ORDER BY e.emp_no DESC;
 
+-- 1952
+SELECT COUNT(et.title) AS "Title_Count", et.title
+INTO firstround_retirees
+FROM employee_titles as et
+WHERE (et.birth_date BETWEEN '1952-01-01' AND '1952-12-31')
+GROUP BY et.title
+ORDER BY "Title_Count" DESC;
 
-SELECT COUNT(ut.title) as "Title Count", 
-	ut.title
-	e.birth_date
---INTO 1953_titles
-FROM unique_titles as ut, employees as e
-WHERE (e.birth_date BETWEEN '1953-01-01' AND '1953-12-31')
-GROUP BY ut.title
-ORDER BY "Title Count" DESC;
+--1953
+SELECT COUNT(et.title) AS "Title_Count", et.title
+INTO secondround_retirees
+FROM employee_titles as et
+WHERE (et.birth_date BETWEEN '1953-01-01' AND '1953-12-31')
+GROUP BY et.title
+ORDER BY "Title_Count" DESC;
 
-SELECT COUNT(ut.title) as "Title Count", 
-	ut.title
-	e.birth_date
---INTO 1954_titles
-FROM unique_titles as ut, employees as e
-WHERE (e.birth_date BETWEEN '1954-01-01' AND '1954-12-31')
-GROUP BY ut.title
-ORDER BY "Title Count" DESC;
+--1954
+SELECT COUNT(et.title) AS "Title_Count", et.title
+INTO thirdround_retirees
+FROM employee_titles as et
+WHERE (et.birth_date BETWEEN '1954-01-01' AND '1954-12-31')
+GROUP BY et.title
+ORDER BY "Title_Count" DESC;
 
-SELECT COUNT(ut.title) as "Title Count", 
-	ut.title
-	e.birth_date
---INTO 1955_titles
-FROM unique_titles as ut, employees as e
-WHERE (e.birth_date BETWEEN '1955-01-01' AND '1955-12-31')
-GROUP BY ut.title
-ORDER BY "Title Count" DESC;
+--1955
+SELECT COUNT(et.title) AS "Title_Count", et.title
+INTO fourthround_retirees
+FROM employee_titles as et
+WHERE (et.birth_date BETWEEN '1955-01-01' AND '1955-12-31')
+GROUP BY et.title
+ORDER BY "Title_Count" DESC;
